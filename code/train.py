a94f79a1817b7851c0358aaf72197271f9cc85d9
yuvaramsingh94 <yuvaramsingh94@gmail.com>
2025-04-02 15:29:29 +0530

    fix

--- code/train.py
+++ code/train.py
@@ -37,6 +37,7 @@ def dir_to_df(PATH: os.path) -> pd.DataFrame:
             image_path = [
                 os.path.join(os.path.join(PATH, label), i)
                 for i in os.listdir(os.path.join(PATH, label))
+                if "jpg" in i
             ]
             label_list = [label] * len(image_path)
             data_dict["image_path"].extend(image_path)
