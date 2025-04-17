13a06d7cded2498ac5dc4e17bef3b4e599d57659
yuvaramsingh94 <yuvaramsingh94@gmail.com>
2025-04-17 14:00:16 +0530

    scrath train

--- Part_a/train.py
+++ Part_a/train.py
@@ -5,17 +5,33 @@ from lightning.pytorch.loggers import WandbLogger
 import pandas as pd
 from torchvision.transforms.v2 import Normalize
 import os
+import wandb
 from sklearn.model_selection import StratifiedShuffleSplit
 from lightning import Trainer, seed_everything
 from dataloader import CustomImageDataset
 from utils import dir_to_df
 from lightning.pytorch.callbacks import ModelCheckpoint
+import argparse
 
 SEED = 5
 seed_everything(SEED, workers=True)
 
+parser = argparse.ArgumentParser(description="HP sweep")
+parser.add_argument(
+    "--kaggle", action="store_true", help="Set this flag to true if its kaggle"
+)
+parser.add_argument(
+    "--colab", action="store_true", help="Set this flag to true if its colab"
+)
+parser.add_argument("-w", "--wandb_key", type=str, help="wandb key")
+args = parser.parse_args()
+
 
 DATASET_PATH = os.path.join("dataset", "inaturalist_12K")
+if args.kaggle:
+    DATASET_PATH = os.path.join(
+        "/kaggle", "input", "intro-to-dl-a2-d", "inaturalist_12K"
+    )
 TRAIN_PATH = os.path.join(DATASET_PATH, "train")
 TEST_PATH = os.path.join(DATASET_PATH, "val")
 
@@ -60,6 +76,23 @@ val_loader = DataLoader(
     pin_memory=True,
     # num_workers=2,
 )
+
+if args.kaggle:
+    ## Kaggle secret
+    from kaggle_secrets import UserSecretsClient
+
+    secret_label = "wandb_api_key"
+    wandb_key = UserSecretsClient().get_secret(secret_label)
+    wandb.login(key=wandb_key)
+elif args.colab:
+    ## Kaggle secret
+    # from google.colab import userdata
+
+    # secret_label = "wandb_api_key"
+    # wandb_key = userdata.get(secret_label)
+    wandb.login(key=args.wandb_key)
+else:
+    wandb.login()
 
 # wandb.login()
 # wandb.init(
