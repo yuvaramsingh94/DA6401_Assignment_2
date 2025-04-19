36bba7339a0460243ea60cab8707da15ad6ac33e
yuvaramsingh94 <yuvaramsingh94@gmail.com>
2025-04-19 21:51:55 +0530

    pred

--- Part_a/predictions.py
+++ Part_a/predictions.py
@@ -94,10 +94,11 @@ wandb.init(
 
 prob_list = []
 pred_list = []
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 for i in tqdm(range(len(test_df))):
     imgs, _ = test_dataset.__getitem__(i)
-    logits = lit_model(torch.unsqueeze(imgs, 0).to("cuda:0"))
+    logits = lit_model(torch.unsqueeze(imgs, 0).to(device))
     prob = F.softmax(logits, dim=1)  # .cpu().detach().numpy()
     preds = torch.argmax(prob, dim=1).cpu().detach().numpy()
     prob = prob.cpu().detach().numpy()
